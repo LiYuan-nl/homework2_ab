c73fa359dc006175235d87915b94bec513c251de
LiYuan-nl <573772756@qq.com>
2022-12-05 16:10:48 +0800

    Update RestController.java
    url a+b

--- src/main/java/com/example/homework2/controller1/RestController.java
+++ src/main/java/com/example/homework2/controller1/RestController.java
@@ -1,5 +1,6 @@
 package com.example.homework2.controller1;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,9 +9,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/demo_ab")
 public class RestController{
     @RequestMapping(value = "/sum1", method = RequestMethod.GET)
-    public Integer index(@RequestParam("a") Integer a, @RequestParam("b")Integer b) {
-        int sum = a + b;
-        System.out.println("a = "+a+",b = " +b+",a + b ="+sum);
-        return sum;
+    public Integer index1(@RequestParam("a") Integer a, @RequestParam("b")Integer b) {
+        int sum1 = a + b;
+        System.out.println("a = "+a+",b = " +b+",a + b ="+sum1);
+        return sum1;
+    }
+    @RequestMapping("/sum2/{a}/{b}")
+    public Integer index2(@PathVariable("a") Integer a, @PathVariable("b")Integer b) {
+        int sum2 = a + b;
+        System.out.println("a = "+a+",b = " +b+",a + b ="+sum2);
+        return sum2;
     }
 }
